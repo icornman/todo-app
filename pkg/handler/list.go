15bea13d9e0b1381757b069ad615e0656ed8520d
icornman <vlad.ischuck2006@gmail.com>
2022-05-26 21:01:11 +0300

    supported: deleting lists

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -76,5 +76,24 @@ func (h *Handler) updateList(c *gin.Context) {
 }
 
 func (h *Handler) deleteList(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		return
+	}
 
+	listId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid id param")
+		return
+	}
+
+	err = h.services.TodoList.Delete(userId, listId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, statusResponse{
+		Status: "deleted successfully",
+	})
 }
--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -9,6 +9,10 @@ type errorResponse struct {
 	Message string `json:"message"`
 }
 
+type statusResponse struct {
+	Status string `json:"status"`
+}
+
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
 	log.Printf(message)
 	c.AbortWithStatusJSON(statusCode, errorResponse{message})
