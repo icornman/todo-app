2adeff8053c2c50483c089da82c9703e3d3d7e9a
icornman <vlad.ischuck2006@gmail.com>
2022-05-29 18:23:33 +0300

    refactored: auth_postgres.go

--- pkg/repository/auth_postgres.go
+++ pkg/repository/auth_postgres.go
@@ -18,7 +18,7 @@ func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 func (r *AuthPostgres) CreateUser(user todo.User) (int, error) {
 	var id int
 
-	query := fmt.Sprintf("INSERT INTO %s (name, username, password_hash) values ($1, $2, $3) RETURNING id", usersTable)
+	query := fmt.Sprintf("INSERT INTO %s (name, username, password) values ($1, $2, $3) RETURNING id", usersTable)
 	row := r.db.QueryRow(query, user.Name, user.Username, user.Password)
 
 	if err := row.Scan(&id); err != nil {
@@ -30,7 +30,7 @@ func (r *AuthPostgres) CreateUser(user todo.User) (int, error) {
 
 func (r *AuthPostgres) GetUser(username, password string) (todo.User, error) {
 	var user todo.User
-	query := fmt.Sprintf("SELECT id FROM %s WHERE username=$1 AND password_hash=$2", usersTable)
+	query := fmt.Sprintf("SELECT id FROM %s WHERE username=$1 AND password=$2", usersTable)
 	err := r.db.Get(&user, query, username, password)
 
 	return user, err
